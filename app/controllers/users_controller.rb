710f1b63fe523ee887aba9d98b716fa7c0f0b7be
Felipe Llerena <felipe.llerena@koombea.com>
2018-08-09 13:14:19 -0500

    edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,17 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
   private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :address, :city, :state, :cellphone, :phone, :company)
