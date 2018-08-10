1273523da290e8557b767baa71b5160523b6a586
Felipe Llerena <felipe.llerena@koombea.com>
2018-08-10 15:47:35 -0500

    create companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,8 +4,35 @@ class CompaniesController < ApplicationController
     @companies = Company.all
   end
 
+  def new
+    @company = Company.new  
+  end
+
   def show
     @company = Company.find(params[:id])
+  end
+
+  def create 
+    @company = Company.new(company_params)
+    if @company.save
+      redirect_to @company
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(
+      :name,
+      :commercial_activity,
+      :address,
+      :city,
+      :state,
+      :cellphone,
+      :phone
+    )
   end
   
 end
