01ba64e61d4bc09d082bd799486cc15e559e1f85
Felipe Llerena <felipe.llerena@koombea.com>
2018-08-10 09:41:22 -0500

    email's validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :address, presence: true, uniqueness: true
+  validates :address, presence: true
   validates :password, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/ }
   validates :city, presence: true
   validates :state, presence: true
   validates :cellphone, presence: true
