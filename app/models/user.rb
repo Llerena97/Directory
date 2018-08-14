e6f091038a99d5bb19636f4e55503373d335f4a2
Felipe Llerena <felipe.llerena@koombea.com>
2018-08-14 16:58:03 -0500

    add sign in with Google

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,16 @@ class User < ApplicationRecord
 
   validates :first_name, :last_name, :address, :city, :state, :cellphone, presence: true
   belongs_to :company, optional: true
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.email = auth.info.email
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!(:validate => false)
+    end
+  end
 end
