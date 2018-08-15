addb3dd96ebfb98fc49947972a027c2b0b77980a
Felipe Llerena <felipe.llerena@koombea.com>
2018-08-15 17:26:56 -0500

    importing contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do  
+  match "/oauth2callback" => "import#index", :via => [:get]
   resources :users
   resources :companies
   devise_for :users, path: 'accounts', controllers: {omniauth_callbacks: "omniauth_callbacks"}
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root to: "companies#index" 
 end
